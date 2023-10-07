3cfa566aef1a8d6cbc943869139f3eb7a6112898
Grigorii Praskura <gregorypraskura@hotmail.com>
2023-10-07 17:24:14 +0300

    Add fetch from DB

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import asyncpg
+import json
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
 from settings import Settings
@@ -12,4 +14,10 @@ async def health_check():
 
 @app.get("/get_entities")
 async def get_entities():
-    return {"data": null}
+    conn = await asyncpg.connect(dsn=settings.database_dsn)
+
+    result = [dict(v) for v in await conn.fetch('SELECT * FROM wild_animals')]
+
+    await conn.close()
+
+    return {"data": result}
--- settings.py
+++ settings.py
@@ -2,8 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 	replica: int = 0
-	database_url: str
-
+	database_dsn: str
 	class Config:
 		env_file = '.env'
 		env_file_encoding = 'utf-8'
