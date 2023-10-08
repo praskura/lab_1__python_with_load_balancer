733c9acde1a14fd58db13c01f5473a6a7287327f
Grigorii Praskura <gregorypraskura@hotmail.com>
2023-10-08 16:24:56 +0300

    Add load test for get_entities endpoint

--- testing/locustfile.py
+++ testing/locustfile.py
@@ -4,3 +4,8 @@ class HealthCheckUser(HttpUser):
     @task
     def load_health_check(self):
         self.client.get("/health_check")
+
+class DatabaseUser(HttpUser):
+    @task
+    def load_get_entities(self):
+        self.client.get("/get_entities")
